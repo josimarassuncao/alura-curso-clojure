ccf780737e1ff0b67acbcafe1b2f66f7517910d8
Josimar Assuncao <josimar.assuncao@nubank.com.br>
2021-05-10 15:45:59 -0300

    conteúdo sobre operações sobre listas

--- src/alura_curso/price.clj
+++ src/alura_curso/price.clj
@@ -6,7 +6,7 @@
   (let [percent_value (/ 9 10.0)]
     (* percent_value original_value)))
 
-(defn apply-discount
+(defn apply-discount?
   "Check whether should apply a discount"
   [price]
   (> price 100))
@@ -14,5 +14,25 @@
 (defn get-prices []
   (let [list [35, 122, 780, 56, 157, 972]]
     list))
+; Collections are indexed from 0
+; (list 0) // gets first element
+; (get list 0) // gets first element
 
-(map discounted-price (filter apply-discount (get-prices)))
+; Using the function get avoids getting errors of non-existent indexes
+; It is also possible to provide a default value in case of an non-existent index
+; (get list 17 -1) // -1 is the default value
+
+; (update collection index function)
+; (update prices 0 inc)
+; (update prices 1 dec)
+
+(map discounted-price (filter apply-discount? (get-prices)))
+
+(reduce + (filter apply-discount? (get-prices)))
+
+(defn my-add [val1, val2] (+ val1 val2))
+
+; Throws an error due to the number of arguments for the function my-add
+; However when using the function + the error is not thrown
+; (reduce my-add [])
+(reduce + [])
