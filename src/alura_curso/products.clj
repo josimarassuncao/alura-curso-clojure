28812fc51261a82c9ab9fc2b7c5269fe6ee907c8
Josimar Assuncao <josimar.assuncao@nubank.com.br>
2021-05-10 18:40:10 -0300

    adiciona composição e exemplos combinando símbolos e funções anônimas

--- src/alura_curso/products.clj
+++ src/alura_curso/products.clj
@@ -3,8 +3,11 @@
 (def list-of-products {:backpack {:quantity 10, :price 59.99} ,
                        :shoes {:quantity 10, :price 159.99},
                        :belt {:quantity 10, :price 19.99},
-                       :hat {:quantity 10 :price 109.99}
-                       :shirt {:quantity 10, :price 129.99}})
+                       :hat {:quantity 10 :price 109.99},
+                       :socks {:quantity 10, :price 9.99},
+                       :shirt {:quantity 10, :price 129.99},
+                       :stickers {:quantity 100},
+                       :stamps {:quantity 100, :price 0.00}})
 
 (defn get-products-data []
   "returns the list of products and quantities to sell"
@@ -33,13 +36,31 @@
 
 (defn calculate-item
   "multiplies quantity and price of an item"
-  [[key value]]
-  (println value)
+  [value]
+  ;(println value)
   (* (:price value) (:quantity value)))
 
 (defn inventory-balance []
   "calculates the value of the whole of the inventory"
   (->> (get-products-data)
+       vals
       (map calculate-item)
       (reduce +)))
 
+(defn for-free?
+  "check if the item has price 0"
+  [data]
+  ;(println data)
+  (<= (get data :price 0) 0))
+
+(defn get-free-stuff []
+  "returns the free products in the list"
+  (->> (get-products-data)
+       (filter #(for-free? (second %)))))
+
+(def paid? (comp not for-free?))
+
+(defn get-paid-stuff []
+  "returns the paid items from the list"
+  (->> (get-products-data)
+       (filter #(paid? (second %)))))
