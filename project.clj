f3d66853e32b7d45aa422abb7eaac30a8cdf64f3
Josimar Assuncao <josimar.assuncao@nubank.com.br>
2021-05-27 10:09:42 -0300

    adds dependency do kafka client intending to simulate in a easy way what was shown in alura course but with clojure

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [com.datomic/datomic-pro "1.0.6269"]]
-  ;; com.datomic/client-pro "1.0.6269" "0.9.66"
+                 [com.datomic/datomic-pro "1.0.6269"]
+                 [org.apache.kafka/kafka-clients "2.8.0"]]
   :repl-options {:init-ns alura-curso.core})
