ab7482ad4f7d1b399e7e258dfee65b8f5ccb86f3
Josimar Assuncao <josimar.assuncao@nubank.com.br>
2021-05-31 14:35:38 -0300

    changes to use atom instead of pure global def

--- src/alura_curso/logistics/shipping/logic.clj
+++ src/alura_curso/logistics/shipping/logic.clj
@@ -1,18 +1,22 @@
 (ns alura-curso.logistics.shipping.logic)
 
+(def ^:private queue-limit 5)
+
+(defn- can-item-be-added?
+  "Evaluates whether an item can be added to shipping queue"
+  [main-queue department]
+  (-> (department main-queue)
+      (count)
+      (< queue-limit)))
 
 (defn add-shipping
   "Adds a product to the queue and then shipped"
   [main-queue department item]
   ;; FIXME: when department does not exist is being build with incorrect queue data structure
-  (let [queue (department main-queue)
-        current-size (count queue)
-        fits-more? (< current-size 5)]
-    (if fits-more?
+  (let [can-add (can-item-be-added? main-queue department)]
+    (if can-add
       (update main-queue department conj item)
-      main-queue)))
-
-
+      (throw (ex-info "The queue is full" {:adding-item item})))))
 
 (defn shipping-prepared!
   "Removes a product from the queue"
