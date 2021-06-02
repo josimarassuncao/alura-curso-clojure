d47a420b1ac99f90b9b7a4535c28793967ffb666
Josimar Assuncao <josimar.assuncao@nubank.com.br>
2021-06-02 10:46:56 -0300

    adds content related to schema validation using clojure and schema.core

--- src/alura_curso/logistics/carrier/logic.clj
+++ src/alura_curso/logistics/carrier/logic.clj
@@ -19,7 +19,7 @@
 
 ;(def RatingLimit (s/pred rating-limit-ok? 'ratings-limit))
 
-(def CarrierSchema
+(s/def ^:private CarrierSchema
   "Schema for a carrier"
   {:id s/Str
    :name s/Str
@@ -36,41 +36,53 @@
    :name name
    :rating rating})
 
+; Following lines throw errors due to -1 and 10.01 not being between 0 and 10
 ;(pprint (new-carrier "CA-307159425" "ACME Transports" -1))
 ;(pprint (new-carrier "CA-307159425" "ACME Transports" 10.01))
 (pprint (new-carrier "CA-307159425" "ACME Transports" 8))
 
-(defn- adds-carrier
-  [carriers-list carrier]
-  (if-let [id (:id carrier)]
+(s/defschema CarriersList {s/Str CarrierSchema})
+
+(s/defn adds-carrier :- CarriersList
+  [carriers-list :- CarriersList
+   carrier :- CarrierSchema]
+  (let [id (:id carrier)]
     (assoc carriers-list id carrier)
-    (throw (ex-info "Carrier with no :id provided" {:carrier carrier}))
     ))
 
-(defn- add-carrier-destiny
-  [carriers-list carrier-id destinies]
+(s/defschema CarriersDestiny {s/Str [s/Str]})
+
+(s/defn add-carrier-destiny :- CarriersDestiny
+  [carriers-destinies :- CarriersDestiny
+   carrier-id :- s/Str
+   destinies :- [s/Str]]
   ;; TODO: is it necessary to ensure having the same data always?
   ;; How it is written the first time is a vector, after an update it is a seq
-  (if (contains? carriers-list carrier-id)
-    (update carriers-list carrier-id concat destinies)
-    (assoc carriers-list carrier-id destinies))
+  (if (contains? carriers-destinies carrier-id)
+    (update carriers-destinies carrier-id concat destinies)
+    (assoc carriers-destinies carrier-id destinies))
   )
 
-(defn- get-destinies-for-carrier
-  [carriers-list carrier-id]
-  (get carriers-list carrier-id))
+(s/defn get-destinies-for-carrier :- [s/Str]
+  [carriers-destiny-list :- CarriersDestiny
+   carrier-id :- s/Str]
+  (get carriers-destiny-list carrier-id))
 
 (defn test-carriers []
   (let [carrier-1 (new-carrier "CA-307159425" "ACME Transports" 8.1M)
         carrier-2 (new-carrier "CA-325240231" "Carrier Eased" 7.7)
         carrier-3 (new-carrier "CA-853891712" "We Carry All Stuff" 9.5)
         carriers (reduce adds-carrier {} [carrier-1 carrier-2 carrier-3])
+        ;; Throws an error due to s/Str not being an CarrierSchema
+        ;; carriers (reduce adds-carrier {} ["CA-853891712" carrier-2 carrier-3])
 
         destinies {}
         destinies (add-carrier-destiny destinies "CA-307159425" ["dest-1" "dest-2" "dest-3"])
         destinies (add-carrier-destiny destinies "CA-853891712" ["dest-10" "dest-20" "dest-30"])
         destinies (add-carrier-destiny destinies "CA-307159425" ["dest-5" "dest-6"])
         destinies (add-carrier-destiny destinies "CA-325240231" ["dest-15" "dest-16" "dest-13"])
+        ;; Throws an exception given carrier-1 not being an s/Str
+        ;; destinies (add-carrier-destiny destinies carrier-1 ["dest-15" "dest-16" "dest-13"])
         ]
 
       (println carriers)
@@ -81,6 +93,8 @@
       (pprint (get-destinies-for-carrier destinies "CA-325240231"))
       (println "destinies for carrier 3 > \"CA-853891712\"")
       (pprint (get-destinies-for-carrier destinies "CA-853891712"))
+      ;; Throws an error because of the carrier-1 not being a s/Str
+      ;;(pprint (get-destinies-for-carrier destinies carrier-1))
     ))
 
 (test-carriers)
